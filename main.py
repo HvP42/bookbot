b631062f7646de5f0386a5916a86025781d6db25
Heinz van Pee <heinz.vanpee@gmail.com>
2024-11-13 06:57:09 +0100

    finished

--- main.py
+++ main.py
@@ -6,9 +6,34 @@ def main():
     book_path = "books/frankenstein.txt"
     contents = read_book(book_path)
     words = contents.split()
-    print(len(words))
-    print(num_chars(contents))
+    print_report(book_path)
     
+def print_report(book_path):
+    print("--- Begin report of " + book_path + " ---")
+    contents = read_book(book_path)
+    words = contents.split()
+    print(f"{len(words)} found in the document")
+    chars = num_chars(contents)
+    l = list_of_dict(chars)
+    l.sort(reverse=True, key=sort_on)
+    for d in l:
+        buchstabe = d["name"]
+        nummer = d["num"]
+        print(f"The {buchstabe} character was found {nummer} times")
+    
+    
+def list_of_dict(char_dict):
+    result = []
+    for key in char_dict:
+        val = char_dict[key]
+        x = {}
+        x["name"] = key
+        x["num"] = char_dict[key]
+        result.append(x)
+    return result 
+    
+def sort_on(dict):
+    return dict["num"]
 
 def read_book(path):
     with open(path) as f:
